61e636892f800e097aacb994c9bd6cc5b20e2c45
No3Eyes <nothreeeye@gmail.com>
2023-01-04 16:47:25 +0800

    資料庫建好後的環境建置

--- MVC/WebApi/Controllers/MemberController.cs
+++ MVC/WebApi/Controllers/MemberController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using WebApi.DTO;
 using WebApi.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -8,52 +7,55 @@ namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductController : ControllerBase
+    public class MemberController : ControllerBase
     {
-        private readonly myDemoDBContext _context;
+        private readonly SweetMouthContext _context;
 
-        public ProductController(myDemoDBContext context)
+        public MemberController(SweetMouthContext context)
         {
             _context = context;
         }
 
 
-        // GET: api/<ProductController>
+
+        // GET: api/<MemberController>
         [HttpGet]
-        public IEnumerable<Product> Get()
+        public IEnumerable<Member> Get()
         {
-            return _context.Product.Select(x =>
-            new Product
+            return _context.Member.Select(a =>
+            new Member
             {
-                商品id=x.商品id,
-                商品名稱=x.商品名稱,
-                商品份數口味=x.商品份數口味,
-                商品定價=x.商品定價
+                MemberId= a.MemberId,
+                Name= a.Name,
+                NickName= a.NickName,
+                Email= a.Email,
+                PhoneNumber= a.PhoneNumber,
+                Password= a.Password,
+                BirthDay= a.BirthDay
             }
             );
         }
 
-
-        // GET api/<ProductController>/5
+        // GET api/<MemberController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/<ProductController>
+        // POST api/<MemberController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/<ProductController>/5
+        // PUT api/<MemberController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/<ProductController>/5
+        // DELETE api/<MemberController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
