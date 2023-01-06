6313d2c57c10aa5598254d612a3f9923f8c35324
LiyaTsai <116324888+LiyaTsai@users.noreply.github.com>
2023-01-06 15:41:12 +0800

    head/標題/title顯示檔名

--- MVC/MVC/Controllers/HomeController.cs
+++ MVC/MVC/Controllers/HomeController.cs
@@ -15,16 +15,19 @@ namespace MVC.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.Title = "首頁";
             return View();
         }
 
         public IActionResult product()
         {
+            ViewBag.Title = "甜點品項";
             return View();
         }
 
         public IActionResult contact()
         {
+            ViewBag.Title = "聯絡我們";
             return View();
         }
 
@@ -35,45 +38,55 @@ namespace MVC.Controllers
 
         public IActionResult Blog()
         {
+            ViewBag.Title = "部落格";
             return View();
         }
 
         public IActionResult News()
         {
+            ViewBag.Title = "最新消息";
             return View();
         }
 
         public IActionResult Cart()
         {
+            ViewBag.Title = "購物車";
             return View();
         }
 
         public IActionResult orderCustomizedCake()
         {
+            ViewBag.Title = "客訂蛋糕";
             return View();
         }
 
         public IActionResult Class()
         {
+            ViewBag.Title = "甜點課程";
             return View();
         }
 
         public IActionResult Class2()
         {
+            ViewBag.Title = "甜點課程";
             return View();
         }
 
         public IActionResult Calendar()
         {
+            ViewBag.Title = "甜點課程";
             return View();
         }
 
         public IActionResult DemoProduct() 
         {
+
+            ViewBag.Title = "甜點清單DEMO";
             return View();
         }
         public IActionResult memberfrom()
         {
+            ViewBag.Title = "會員表單";
             return View();
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
