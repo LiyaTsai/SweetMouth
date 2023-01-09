d71a22bd75ed8cdc6d3ceab6a5da783e08109879
No3Eyes <nothreeeye@gmail.com>
2023-01-09 23:05:34 +0800

    檔案加回

---
+++ MVC/WebApi/DTO/ProductDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.DTO
+{
+    public class ProductDTO
+    {
+        public int 商品id { get; set; }
+        public string? 商品名稱 { get; set; }
+        public int? 商品定價 { get; set; }
+    }
+}
