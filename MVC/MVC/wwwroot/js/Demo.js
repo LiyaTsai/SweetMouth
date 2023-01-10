1564900dc49ae715e58d09f24f02b4447e8723bd
LiyaTsai <116324888+LiyaTsai@users.noreply.github.com>
2023-01-10 17:08:44 +0800

    demo.js 篩選上架產品

--- MVC/MVC/wwwroot/js/Demo.js
+++ MVC/MVC/wwwroot/js/Demo.js
@@ -19,7 +19,16 @@ var appVue = new Vue({
             axios.get(`${webApiBaseUri}api/Product`).then(a => {
                 _this.ProductInfo = a.data;
                 console.log(_this.ProductInfo)
+                for (i = 0; i < 2; i++)
+                {
+                    console.log(i);
+                };
             })
+            for (let i = 0; i < _this.ProductInfo.lenght; i++) {
+                /*if (_this.ProductInfo[i].avalible == false) {
+                    _this.ProductInfo[i].pop;
+                }*/
+            }
 
         },
         toPD() {
