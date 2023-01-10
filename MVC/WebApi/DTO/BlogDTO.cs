dd3a861672a38736c8fe9aa12d348cc0da9cbb49
LiyaTsai <116324888+LiyaTsai@users.noreply.github.com>
2023-01-10 15:57:20 +0800

    ignore end

---
+++ MVC/WebApi/DTO/BlogDTO.cs
@@ -0,0 +1,18 @@
+﻿using WebApi.Models;
+
+namespace WebApi.DTO
+{
+    public class BlogDTO
+    {
+        public int ArticleID { get; set; }
+        public int MemberID { get; set; }
+        public int Floor { get; set; }
+        public string? Title { get; set; }
+        public string? SubTitle { get; set; }
+        public DateTime? Time { get; set; }
+        public string? Article { get; set; }
+        public string? MemberName { get; set; }
+        public string? NickName { get; set; }
+        //public string? Image { get; set; }
+    }
+}
