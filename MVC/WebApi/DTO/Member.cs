73138ecf10e92ab23b999af92b20bbe0db54e072
LiyaTsai <116324888+LiyaTsai@users.noreply.github.com>
2023-01-11 11:54:45 +0800

    ignore

--- MVC/WebApi/DTO/Member.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApi.DTO
-{
-    public class Member
-    {
-        public int MemberID { get; set; }
-        public string? MemberName { get; set; }
-        public string? NickName { get; set; }
-    }
-}
