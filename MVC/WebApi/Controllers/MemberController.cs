40e2a5b61b45dff8b31a881f38be83dbb08b0205
LiyaTsai <116324888+LiyaTsai@users.noreply.github.com>
2023-01-11 18:09:49 +0800

    data reinstall

---
+++ MVC/WebApi/Controllers/MemberController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.DTO;
+using WebApi.Models;
+using Member = WebApi.Models.Member;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private readonly SweetMouthContext _context;
+        public MemberController(SweetMouthContext context)
+        {
+            _context = context;
+        }
+
+
+
+        // GET: api/<MemberController>
+        [HttpGet]
+        public IEnumerable<Member> Get()
+        {
+            return _context.Member.Include(b=>b.Order).Select(a =>
+            new Member
+            {
+                MemberId= a.MemberId,
+                Name= a.Name,
+                NickName= a.NickName,
+                Email= a.Email,
+                PhoneNumber= a.PhoneNumber,
+                Password= a.Password,
+                BirthDay= a.BirthDay,
+            }
+            );
+        }
+
+        // GET api/<MemberController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MemberDTO>> Get(int id)
+        {
+            var mem = await _context.Member.FindAsync(id);
+            if (mem == null)
+            {
+                return NotFound();
+            }
+            return new MemberDTO
+            {
+                MemberID = mem.MemberId,
+                Name = mem.Name,
+                NickName = mem.NickName,
+                Email = mem.Email,
+                PhoneNumber = mem.PhoneNumber,
+                BirthDay = mem.BirthDay
+            };
+        }
+
+        // POST api/<MemberController>
+        [HttpPost]
+        public async Task<Member> Post(MemberDTO mdto)
+        {
+            Member mem = new Member
+            {
+                Name = mdto.Name,
+                NickName = mdto.NickName,
+                Email = mdto.Email,
+                PhoneNumber = mdto.PhoneNumber,
+                BirthDay= mdto.BirthDay
+            };
+            _context.Member.Add(mem);
+            await _context.SaveChangesAsync();
+            return mem;
+        }
+
+        // PUT api/<MemberController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MemberController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MVC/WebApi/Controllers/ProductController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.DTO;
+using WebApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly SweetMouthContext _context;
+
+        public ProductController(SweetMouthContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _context.Product.Select(x =>
+            new Product
+            {
+                ProductName = x.ProductName,
+                Specifications = x.Specifications,
+                Price = x.Price,
+                ImageName = x.ImageName,
+                Avalible = x.Avalible
+            }
+            );
+        }
+
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{productName}-{specifications}")]
+        public async Task<ActionResult<Product>> GetPD([FromRoute] string productName, [FromRoute] string specifications)
+        {
+            var pd = await _context.Product.FindAsync(productName, specifications);
+
+            return new Product
+            {
+                //productName = pd.ProductName,
+                //price = pd.Price,
+                //Specifications = pd.Specifications,
+            };
+
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
