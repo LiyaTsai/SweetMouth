5d2a678d8650e643909c2980c4ed22afde4920c6
ABAi <80210850+ABai-fefeder@users.noreply.github.com>
2023-01-17 15:16:46 +0800

    新增後台demo

--- MVC/backStage/wwwroot/js/template.js
+++ MVC/backStage/wwwroot/js/template.js
@@ -29,13 +29,75 @@ var appVue = new Vue({
                 item3: 'item3',
             }
         ],
+        p_item: [],
+        item1: null,
+        item2: null,
+        item3: null,
+        // 容器
+        item1_E: null,
+        item2_E: null,
+        item3_E: null,
     },
     mounted: function () {
         _this = this;
+        _this.start();
     },
     methods: {
-        edit: function () {
-            Edit = true;
-        }
+        start: function () {
+            let _this = this;
+            let List = [];
+            for (i = 0; i < _this.p.length; i++) {
+                let item = {}
+                item = _this.p[i]
+                List.push(item)
+            }
+            _this.p_item = List;
+        },
+        edit: function (id) {
+            let _this = this;
+            let List = [];
+            for (i = 0; i < _this.p_item.length; i++) {
+                let item = {}
+                item = _this.p_item[i]
+                if (id == _this.p_item[i].id) {
+                    item.Edit = true;
+                    _this.item1_E = item.item1;
+                    _this.item2_E = item.item2;
+                    _this.item3_E = item.item3;
+                } else {
+                    item.Edit = false;
+                }
+                List.push(item);
+            }
+            _this.p_item = List;
+        },
+        cancel: function () {
+            let _this = this;
+            let List = [];
+            for (i = 0; i < _this.p_item.length; i++) {
+                let item = {}
+                item = _this.p_item[i]
+                if (item.Edit == true) {
+                    item.Edit = false;
+                    item.item1 = _this.item1_E;
+                    item.item2 = _this.item2_E;
+                    item.item3 = _this.item3_E;
+                }
+                List.push(item);
+            }
+            _this.p_item = List;
+        },
+        showModal: function () {
+            $("#insertModal").modal('show');
+        },
+        delete: function (id) {
+            console.log(id)
+            // let _this = this;
+            // var ret = confirm("確定要刪除嗎?");
+            // if (ret == true) {
+
+            //     _this.start();
+            // }
+        },
     },
 })
