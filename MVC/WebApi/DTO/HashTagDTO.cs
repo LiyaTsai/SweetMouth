e91e9cead28b5f39e65bb372a93a650d04b37563
steve <stevemk1986@gmail.com>
2023-01-17 09:38:18 +0800

    1/17修正ignore後09:37

---
+++ MVC/WebApi/DTO/HashTagDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi.DTO
+{
+    public class HashTagDTO
+    {
+        public string? ProductName { get; set; }
+        public string? Specifications { get; set; }
+        public string? HashTag1 { get; set; }
+        public int? Price { get; set; }
+        public string? ImageName { get; set; }
+        public bool? Avalible { get; set; }
+    }
+}
---
+++ MVC/WebApi/DTO/ProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.DTO
+{
+    public class ProductDTO
+    {
+        public string ProductName { get; set; }
+        public string Specifications { get; set; }
+        public int? Price { get; set; }
+        public string ImageName { get; set; }
+        public bool? Avalible { get; set; }
+    }
+}
