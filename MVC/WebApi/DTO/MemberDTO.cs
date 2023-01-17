3d14e6306b57f49775427735c8e03c900263d20d
steve <stevemk1986@gmail.com>
2023-01-17 09:29:55 +0800

    git ignore

---
+++ MVC/WebApi/DTO/MemberDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi.DTO
+{
+    public class MemberDTO
+    {
+        public int MemberID { get; set; }
+        public string? Name { get; set; }
+        public string? NickName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Password { get; set; }
+        public DateTime? BirthDay { get; set; }
+    }
+}
