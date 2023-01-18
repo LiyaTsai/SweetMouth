72bbd7b4b2594326a6296b8ab93a8ddf39f3d0fa
No3Eyes <nothreeeye@gmail.com>
2023-01-18 21:25:59 +0800

    1/18 修正swagger

--- MVC/WebApi/Controllers/ProductController.cs
+++ MVC/WebApi/Controllers/ProductController.cs
@@ -18,11 +18,11 @@ namespace WebApi.Controllers
             _context = context;
         }
 
-        // GET: Products
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Product.ToListAsync());
-        }
+        //// GET: Products
+        //public async Task<IActionResult> Index()
+        //{
+        //    return View(await _context.Product.ToListAsync());
+        //}
 
         // GET: api/<ProductController>
         [HttpGet]
@@ -113,119 +113,119 @@ namespace WebApi.Controllers
         {
         }
 
-        // GET: Products/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+        //// GET: Products/Create
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
 
         // POST: Products/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ProductName,Specifications,Price,ImageName,Avalible")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(product);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(product);
-        }
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("ProductName,Specifications,Price,ImageName,Avalible")] Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(product);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(product);
+        //}
 
         // GET: Products/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null || _context.Product == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Edit(string id)
+        //{
+        //    if (id == null || _context.Product == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var product = await _context.Product.FindAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return View(product);
-        }
+        //    var product = await _context.Product.FindAsync(id);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(product);
+        //}
 
         // POST: Products/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("ProductName,Specifications,Price,ImageName,Avalible")] Product product)
-        {
-            if (id != product.ProductName)
-            {
-                return NotFound();
-            }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(string id, [Bind("ProductName,Specifications,Price,ImageName,Avalible")] Product product)
+        //{
+        //    if (id != product.ProductName)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(product);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductExists(product.ProductName))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(product);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(product);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!ProductExists(product.ProductName))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(product);
+        //}
 
         // GET: Products/Delete/5
-        public async Task<IActionResult> Delete(string id)
-        {
-            if (id == null || _context.Product == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Delete(string id)
+        //{
+        //    if (id == null || _context.Product == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var product = await _context.Product
-                .FirstOrDefaultAsync(m => m.ProductName == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
+        //    var product = await _context.Product
+        //        .FirstOrDefaultAsync(m => m.ProductName == id);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(product);
-        }
+        //    return View(product);
+        //}
 
         // POST: Products/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            if (_context.Product == null)
-            {
-                return Problem("Entity set 'SweetMouthContext.Product'  is null.");
-            }
-            var product = await _context.Product.FindAsync(id);
-            if (product != null)
-            {
-                _context.Product.Remove(product);
-            }
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(string id)
+        //{
+        //    if (_context.Product == null)
+        //    {
+        //        return Problem("Entity set 'SweetMouthContext.Product'  is null.");
+        //    }
+        //    var product = await _context.Product.FindAsync(id);
+        //    if (product != null)
+        //    {
+        //        _context.Product.Remove(product);
+        //    }
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
-        private bool ProductExists(string id)
-        {
-            return _context.Product.Any(e => e.ProductName == id);
-        }
+        //private bool ProductExists(string id)
+        //{
+        //    return _context.Product.Any(e => e.ProductName == id);
+        //}
     }
 }
