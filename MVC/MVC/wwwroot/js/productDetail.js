e16a4f31cf749124da1830c6cf3195d3df1af2c9
LiyaTsai <116324888+LiyaTsai@users.noreply.github.com>
2023-01-24 21:58:18 +0800

    ProductDetail to cart failed

--- MVC/MVC/wwwroot/js/productDetail.js
+++ MVC/MVC/wwwroot/js/productDetail.js
@@ -19,10 +19,12 @@ var appVue = new Vue({
         price: "",
         flavor: "",
         size: "",
+        sizeArray: "",
         imageName: "",
         tag: "",
         description: "",
         tagArray: [],
+        currentSize: "",
     },
     mounted() {
         _this = this;
@@ -38,6 +40,8 @@ var appVue = new Vue({
                 _this.productName = a.data.productName;
                 _this.flavor = a.data.flavor;
                 _this.size = a.data.size.split("|");
+                currentSize = _this.size[0];
+                _this.sizeArray = a.data.size.split("|");
                 _this.tag = a.data.tag;
 
                 //先弄一個放價格的array出來，如果裡面只有一項，就拿來當價格，如果有一項以上就請顧客選擇規格
@@ -50,9 +54,9 @@ var appVue = new Vue({
             });
 
             setTimeout(() => {
-                console.log(_this.PriceArray.length);
+                // console.log("價格陣列長度 " + _this.PriceArray.length);
                 for (let i = 0; i < _this.PriceArray.length; i++) {
-                    console.log(_this.PriceArray[i]);
+                    // console.log("價格 " + _this.PriceArray[i]);
                 }
             }, 200);
         },
@@ -60,10 +64,39 @@ var appVue = new Vue({
             //console.log("在specification裡");
             //console.log(x)
             //console.log(_this.PriceArray[x])
-            this.price = `$${this.PriceArray[x]}`;
+            this.price = `價格  $${this.PriceArray[x]}`;
+            console.log();
+
+            console.log("x = " + x);
+            console.log(_this.size[x]);
+        },
+
+        addToCart(size, price) {
+            let amount = document.querySelector("#productAmount").value;
+            let id = urlProductID;
+            console.log(amount);
+            console.log(size);
+            console.log(price);
+
+            // let _price = price.split("|")[0];
+            // let _size = size.split("|")[0];
+            let session = sessionStorage;
+
+            // if (session[id]) {
+            //     let _amount = 0;
+            //     _amount = session.getItem(id).split("|")[2];
+            //     _amount++;
+            //     value = `${_size}|${_price}|${_amount}`;
+            //     session.removeItem(id);
+            //     session.setItem(id, value);
+            // } else {
+            //     let value = `${_size}|${_price}|${amount}`;
+            //     session.setItem(id, value);
+            // }
         },
     },
 });
+
 // setTimeout(function () {
 //     console.log(appVue.ProductName);
 // }, 100);
